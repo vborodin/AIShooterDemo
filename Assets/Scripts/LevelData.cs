454257a26c8f8ea1466d376e22eb5b46a44db482
vladimir.borodin <vborodin@yandex.ru>
2020-02-22 12:41:32 +0700

    Update LevelData.cs
    - minor fix

--- Assets/Scripts/LevelData.cs
+++ Assets/Scripts/LevelData.cs
@@ -4,11 +4,10 @@ namespace AIShooterDemo
 {
     public class LevelData : MonoBehaviour
     {
-#warning BAD CODE
         [SerializeField] Transform startPosition = null;
         [SerializeField] Transform endPosition = null;
 
-        public Vector3 StartPosition => startPosition.position;
-        public Vector3 EndPosition => endPosition.position;
+        public Vector3 StartPosition => startPosition ? startPosition.position : Vector3.zero;
+        public Vector3 EndPosition => endPosition ? endPosition.position : Vector3.zero;
     }
 }
